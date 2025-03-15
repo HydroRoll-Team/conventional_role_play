06caca8b9a9a1df9932c2c82e4ddba80d49edc5c
HsiangNianian <i@jyunko.cn>
2025-03-15 16:29:12 +0800

    bump: update pyo3 dependency to version 0.24.0 and improve module function signature

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,11 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pyclass]
 pub struct Base {}
 
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
 #[pymodule]
-fn _core(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<Base>()?;
     Ok(())
