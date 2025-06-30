97c274adc780748b986fa5347492616b321c2b13
简律纯 <i@jyunko.cn>
2025-06-30 23:36:16 +0800

    chore: redactor `__version__` detector

--- src/conventionalrp/__init__.py
+++ src/conventionalrp/__init__.py
@@ -1,16 +1,15 @@
+import sys
+from importlib.metadata import version
+
 from . import _core
 
 __all__ = ["_core", "__version__"]
 
-from importlib.metadata import version
-from pkg_resources import get_distribution
-
-try:
+if sys.version_info >= (3, 8):
     # For Python 3.8+
     __version__ = version("conventionalrp")
-except ImportError:
-    try:
-        # For Python < 3.8
-        __version__ = get_distribution("conventionalrp").version
-    except Exception:
-        raise ImportError("Failed to get version")
+elif sys.version_info < (3, 8):
+    from pkg_resources import get_distribution
+
+    # For Python < 3.8
+    __version__ = get_distribution("conventionalrp").version
